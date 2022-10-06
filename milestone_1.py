254f4785bc6731e73d5af03a41450f93f20a31f4
Katy Humphries <katy.humphries96@gmail.com>
2022-10-06 19:06:43 +0100

    Basic guess input

--- milestone_1.py
+++ milestone_1.py
@@ -3,3 +3,14 @@ import random
 word_list = ['pineapple', 'strawberries', 'raspberries', 'peach', 'apple']
 
 print(word_list)
+
+word = (random.choice(word_list))
+
+print(word)
+
+guess = input('Enter a letter')
+
+if len(guess) == 1:
+    print('Good guess!')
+else:
+    print('Oops! That is not a valid input.')
